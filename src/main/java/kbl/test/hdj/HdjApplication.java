869a947b5277c8460036a1778a9208f153653f7b
kbl <siu9068@gmail.com>
2023-07-07 18:00:02 +0900

    feat: 1. 웹 프로젝트 구성

---
+++ src/main/java/kbl/test/hdj/HdjApplication.java
@@ -0,0 +1,13 @@
+package kbl.test.hdj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kbl/test/hdj/HdjApplicationTests.java
@@ -0,0 +1,13 @@
+package kbl.test.hdj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
